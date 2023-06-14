71fd9a541f044ac34a199411c71f533236a76566
Jesse205 <2055675594@qq.com>
2023-06-14 14:29:27 +0800

    chore: add typescript

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,20 +1,23 @@
-export default {
+module.exports = {
     "env": {
         "browser": true,
         "es2021": true
     },
     "extends": [
         "eslint:recommended",
-        "plugin:vue/vue3-essential"
+        "plugin:vue/vue3-essential",
+        "plugin:@typescript-eslint/recommended"
     ],
     "overrides": [
     ],
+    "parser": "@typescript-eslint/parser",
     "parserOptions": {
         "ecmaVersion": "latest",
         "sourceType": "module"
     },
     "plugins": [
-        "vue"
+        "vue",
+        "@typescript-eslint"
     ],
     "rules": {
     }
