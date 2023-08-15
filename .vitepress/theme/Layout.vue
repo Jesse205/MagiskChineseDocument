e27483d62bc928b0b6445a0a707cb440eacfe169
Jesse205 <2055675594@qq.com>
2023-08-16 02:19:24 +0800

    fix: add style to body

--- .vitepress/theme/Layout.vue
+++ .vitepress/theme/Layout.vue
@@ -1,13 +1,26 @@
 <script setup>
 import DefaultTheme from 'vitepress/theme'
 import { useRouter } from 'vitepress'
+import { ref, watchEffect } from 'vue'
 const { Layout } = DefaultTheme
 
 const router = useRouter()
+const isDeltaUrl = ref(false)
+
+watchEffect(() => {
+    isDeltaUrl.value = router.route.path.includes('/delta/')
+})
+
+watchEffect(() => {
+    if (isDeltaUrl.value)
+        document.documentElement.classList.add('delta')
+    else
+        document.documentElement.classList.remove('delta')
+})
 
 </script>
 
 <template>
-    <Layout :class="{ delta: router.route.path.includes('/delta/') }">
+    <Layout>
     </Layout>
 </template>
