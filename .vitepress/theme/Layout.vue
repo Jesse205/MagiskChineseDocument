22ed39665a1e16e9b4b466027b0a52aca85f32c1
Jesse205 <2055675594@qq.com>
2024-04-21 16:54:48 +0800

    fix: 尝试修复delta版本样式问题

--- .vitepress/theme/Layout.vue
+++ .vitepress/theme/Layout.vue
@@ -1,26 +1,26 @@
 <script setup>
-import DefaultTheme from "vitepress/theme"
-import { useRouter } from "vitepress"
-import { ref, watchEffect } from "vue"
+import DefaultTheme from 'vitepress/theme'
+import { useRouter } from 'vitepress'
+import { ref, watchEffect } from 'vue'
 const { Layout } = DefaultTheme
 
 const router = useRouter()
 const isDeltaUrl = ref(false)
 
 watchEffect(() => {
-  isDeltaUrl.value = router.route.path.includes("/delta/")
+  isDeltaUrl.value = router.route.path.includes('/delta/')
 })
 // Delta 主题
 if (!import.meta.env.SSR) {
   watchEffect(() => {
     const classList = document.documentElement.classList
-    const contains = classList.contains("delta")
-    if (isDeltaUrl.value && !contains) classList.add("delta")
-    else if (contains) classList.remove("delta")
+    const contains = classList.contains('delta')
+    if (isDeltaUrl.value && !contains) classList.add('delta')
+    else if (contains) classList.remove('delta')
   })
 }
 </script>
 
 <template>
-  <Layout> </Layout>
+  <Layout :class="{ delta: isDeltaUrl }"> </Layout>
 </template>
